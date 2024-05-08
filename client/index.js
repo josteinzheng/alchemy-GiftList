1de2637ad68f5c512c65b6b4a8219f720cd7b1f7
Jostein Zheng <zhengzhijie@inno-chem.com.cn>
2024-05-08 14:03:33 +0800

    give proof and verify

--- client/index.js
+++ client/index.js
@@ -6,9 +6,15 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  let merkleTee = new MerkleTree(niceList);
+  const args = process.argv.slice(2);
+  const name = args[0];
+  const index = niceList.findIndex(n => n === name);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    proof: merkleTee.getProof(index),
+    leaf: name
   });
 
   console.log({ gift });
